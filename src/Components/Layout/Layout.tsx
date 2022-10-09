1a066bb505bb2484a598adcf0c9b9b9b9b2165b7
Misha-dev <shabatinmichael@mail.ru>
2022-10-10 02:06:28 +0500

    added logout button + logic for logging out

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -1,10 +1,18 @@
 import { Outlet } from "react-router-dom";
+import { useAppSelector } from "../../store/hooks";
+import { LogoutButton } from "../Utils/Buttons/LogoutButton/LogoutButton";
 
 import cl from "./Layout.module.scss";
 
 export const Layout = () => {
+  const user = useAppSelector((state) => state.user);
   return (
     <div className={cl.mainContent}>
+      {user.id && (
+        <div className={cl.menuWrapper}>
+          <LogoutButton text="Logout" type="button" />
+        </div>
+      )}
       <Outlet />
     </div>
   );
