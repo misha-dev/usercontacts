77b73dd3ec902918cc1727b1dc422f0277443f00
Misha-dev <shabatinmichael@mail.ru>
2022-10-11 21:03:56 +0500

    add-contact button

--- src/Components/Contacts/Contacts.tsx
+++ src/Components/Contacts/Contacts.tsx
@@ -1,3 +1,4 @@
+import { AiOutlinePlus } from "react-icons/ai";
 import { SearchInput } from "../Utils/SearchInput/SearchInput";
 import cl from "./Contacts.module.scss";
 
@@ -5,6 +6,11 @@ export const Contacts = () => {
   return (
     <div className={cl.mainWrapper}>
       <SearchInput />
+      <div className={cl.addContact}>
+        <div className={cl.addContactButton}>
+          <AiOutlinePlus className={cl.plus} />
+        </div>
+      </div>
     </div>
   );
 };
