3b095a4a9eb2f21a8d8d70e3fc8a18092f28297e
Misha-dev <shabatinmichael@mail.ru>
2022-10-11 21:13:59 +0500

    fixed bug  with shrinking search input with typed text

--- src/Components/Contacts/Contacts.tsx
+++ src/Components/Contacts/Contacts.tsx
@@ -1,5 +1,7 @@
 import { AiOutlinePlus } from "react-icons/ai";
+
 import { SearchInput } from "../Utils/SearchInput/SearchInput";
+
 import cl from "./Contacts.module.scss";
 
 export const Contacts = () => {
--- src/Components/Utils/SearchInput/SearchInput.tsx
+++ src/Components/Utils/SearchInput/SearchInput.tsx
@@ -1,11 +1,12 @@
 import { BiSearch } from "react-icons/bi";
+
 import cl from "./SearchInput.module.scss";
 
 export const SearchInput = () => {
   return (
     <div className={cl.mainWrapper}>
       <div className={cl.inputWrapper}>
-        <input type="text" />
+        <input required type="text" />
         <BiSearch className={cl.searchLogo}/>
         <div className={cl.placeholder}>Search</div>
       </div>
