6945fdb76f6ce0495286ed0b28749ba402aba658
Misha-dev <shabatinmichael@mail.ru>
2022-10-23 22:19:24 +0500

    added template for contact and contacts components

---
+++ src/Components/Contact/Contact.tsx
@@ -0,0 +1,8 @@
+import { ContactType } from "../../types/ContactType";
+
+import cl from "./Contact.module.scss";
+
+export const Contact = ({id, fullName, phone, type} : ContactType) => {
+
+  return <div className={cl.contactWrapper}>Contact</div>;
+};
--- src/types/ContactType.ts
+++ src/types/ContactType.ts
@@ -1,6 +1,7 @@
 export type PersonType = "Friend" | "Colleague" | "Family";
 
 export type ContactType = {
+  id: number;
   userId: number;
   fullName: string;
   phone: string;
