c90309a1f93d21b18babe162911f5337f032ca1d
Misha-dev <shabatinmichael@mail.ru>
2022-10-25 18:29:58 +0500

    styled contact component

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,3 +1,9 @@
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import DeleteIcon from "@mui/icons-material/Delete";
+import PhoneIcon from "@mui/icons-material/Phone";
+import { IconButton } from "@mui/material";
+import { Stack } from "@mui/system";
+
 import familyIcon from "../../imgs/familyIcon.png";
 import { ContactType } from "../../types/ContactType";
 
@@ -11,7 +17,20 @@ export const Contact = ({ id, fullName, phone, type }: ContactType) => {
         <div className={cl.fullName}>{fullName}</div>
       </div>
 
-      <div className={cl.phoneNumber}>{phone}</div>
+      <div className={cl.phoneOptionsWrapper}>
+        <div className={cl.phoneNumber}>{phone}</div>
+        <Stack spacing={0.5} direction={"row"}>
+          <IconButton aria-label="copy">
+            <ContentCopyIcon />
+          </IconButton>
+          <IconButton aria-label="phone">
+            <PhoneIcon />
+          </IconButton>
+        </Stack>
+        <IconButton className={cl.deleteButton} aria-label="delete">
+          <DeleteIcon />
+        </IconButton>
+      </div>
     </div>
   );
 };
