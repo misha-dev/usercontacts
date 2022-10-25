00154a588bf53ec54de452e56a836cf808b2f942
Misha-dev <shabatinmichael@mail.ru>
2022-10-25 20:01:37 +0500

    added functionality to copy button

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -3,13 +3,16 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import PhoneIcon from "@mui/icons-material/Phone";
 import { IconButton, Link } from "@mui/material";
 import { Stack } from "@mui/system";
+import { useState } from "react";
 
 import familyIcon from "../../imgs/familyIcon.png";
 import { ContactType } from "../../types/ContactType";
+import { MuiCustomizedSnackbar } from "../Utils/MuiCustomizedSnackbar/MuiCustomizedSnackbar";
 
 import cl from "./Contact.module.scss";
 
 export const Contact = ({ id, fullName, phone, type }: ContactType) => {
+  const [openSnackbar, setOpenSnackbar] = useState(false);
   return (
     <div className={cl.contactWrapper}>
       <div className={cl.iconFullName}>
@@ -18,12 +21,20 @@ export const Contact = ({ id, fullName, phone, type }: ContactType) => {
       </div>
 
       <div className={cl.phoneOptionsWrapper}>
+        <MuiCustomizedSnackbar originOfSnackbar={{ horizontal: "left", vertical: "bottom" }} message="Phone copied!" severity="success" autoHide={1500} open={openSnackbar} setOpen={setOpenSnackbar} />
         <div className={cl.phoneNumber}>{phone}</div>
         <Stack spacing={0.5} direction={"row"}>
-          <IconButton aria-label="copy">
+          <IconButton
+            onClick={() => {
+              navigator.clipboard.writeText(phone.replace(/\D/g, "")).then(() => {
+                setOpenSnackbar(true);
+              });
+            }}
+            aria-label="copy"
+          >
             <ContentCopyIcon />
           </IconButton>
-          <Link href="tel:${tel}">
+          <Link href={`tel:${phone}`}>
             <IconButton aria-label="phone">
               <PhoneIcon />
             </IconButton>
