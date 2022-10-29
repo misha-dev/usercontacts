4e6c974643305360a22e80707d96f0c78c2fa4a3
Misha-dev <shabatinmichael@mail.ru>
2022-10-30 00:01:49 +0500

    prepared for heroku

--- src/Utils/constants.ts
+++ src/Utils/constants.ts
@@ -1,2 +1,2 @@
-export const JSON_API = "http://localhost:3001";
-// export const JSON_API = "https://usercontacts.herokuapp.com/api";
+// export const JSON_API = "http://localhost:3001";
+export const JSON_API = "https://usercontacts.herokuapp.com/api";
