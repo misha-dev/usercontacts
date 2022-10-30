ee049f04d60cb686b6ef41bdea5b4d428972e0f1
Misha-dev <shabatinmichael@mail.ru>
2022-10-31 00:50:16 +0500

    improved ui for contact buttons

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -43,11 +43,11 @@ export const Contact = ({ id, fullName, phone, type }: ContactType) => {
             aria-label="copy"
             sx={{ padding: `${isMobile ? "5px" : "8px"}` }}
           >
-            <ContentCopyIcon sx={{ fontSize: `${isMobile ? "1rem" : "1.5rem"} ` }} />
+            <ContentCopyIcon sx={{ fontSize: `${isMobile ? "1.1rem" : "1.5rem"} ` }} />
           </IconButton>
           <Link sx={{ marginLeft: "0px !important" }} href={`tel:${phone}`}>
             <IconButton sx={{ padding: `${isMobile ? "5px" : "8px"}` }} aria-label="phone">
-              <PhoneIcon sx={{ fontSize: `${isMobile ? "1rem" : "1.5rem"} ` }} />
+              <PhoneIcon sx={{ fontSize: `${isMobile ? "1.1rem" : "1.5rem"} ` }} />
             </IconButton>
           </Link>
           <IconButton
@@ -58,7 +58,7 @@ export const Contact = ({ id, fullName, phone, type }: ContactType) => {
             aria-label="delete"
             sx={{ padding: `${isMobile ? "5px" : "8px"}` }}
           >
-            <DeleteIcon sx={{ fontSize: `${isMobile ? "1rem" : "1.5rem"} ` }} />
+            <DeleteIcon sx={{ fontSize: `${isMobile ? "1.1rem" : "1.5rem"} ` }} />
           </IconButton>
         </Stack>
       </div>
