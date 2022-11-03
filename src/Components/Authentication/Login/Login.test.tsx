c8c7a8ee53308e8a1999e8aa86ddf7b07de60b9b
Misha-dev <shabatinmichael@mail.ru>
2022-11-03 15:39:10 +0500

    added mocked useAppSelector function

--- src/Components/Authentication/Login/Login.test.tsx
+++ src/Components/Authentication/Login/Login.test.tsx
@@ -1,7 +1,8 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import * as ReduxHooks from "../../../store/hooks";
+import { testUseAppSelector } from "../../../store/testUseAppSelector";
 
 import { Login } from "./Login";
 
@@ -9,10 +10,10 @@ describe("Login", () => {
   const mockedSelector = jest.spyOn(ReduxHooks, "useAppSelector");
   const mockedDispatch = jest.spyOn(ReduxHooks, "useAppDispatch");
   beforeEach(() => {
-    mockedSelector.mockReturnValue("test select");
+    mockedSelector.mockImplementation(testUseAppSelector);
   });
   it("should render", () => {
-    render(<Login />, { wrapper: BrowserRouter });
+    render(<Login />, { wrapper: HashRouter });
     const email = screen.getByPlaceholderText(/email/i);
     const password = screen.getByPlaceholderText(/password/i);
     const submitLoginButton = screen.getByRole("button");
@@ -23,7 +24,7 @@ describe("Login", () => {
   });
 
   it("should render error messages: Required", async () => {
-    render(<Login />, { wrapper: BrowserRouter });
+    render(<Login />, { wrapper: HashRouter });
     const email = screen.getByPlaceholderText(/email/i);
     const password = screen.getByPlaceholderText(/password/i);
     [email, password].forEach((input) => {
@@ -34,7 +35,7 @@ describe("Login", () => {
     expect(errorRequiredEmailPass.length).toEqual(2);
   });
   it("should render error messages: Invalid email", async () => {
-    render(<Login />, { wrapper: BrowserRouter });
+    render(<Login />, { wrapper: HashRouter });
     const email = screen.getByPlaceholderText(/email/i);
     fireEvent.change(email, { target: { value: "mishaj1jkkj.ru" } });
     fireEvent.blur(email);
@@ -42,7 +43,7 @@ describe("Login", () => {
     expect(errorInvalidEmail).toBeInTheDocument();
   });
   it("should enable login button", () => {
-    render(<Login />, { wrapper: BrowserRouter });
+    render(<Login />, { wrapper: HashRouter });
     const email = screen.getByPlaceholderText(/email/i);
     const password = screen.getByPlaceholderText(/password/i);
     const submitLoginButton = screen.getByRole("button");
---
+++ src/store/testUseAppSelector.ts
@@ -0,0 +1,15 @@
+import { RootState } from ".";
+
+const state: RootState = {
+  user: { id: 1, email: "misha@mail.ru", name: "misha" },
+  contacts: {
+    contacts: [{ fullName: "Misha Shabatin", phone: "+7 (951) 468-85-97", type: "family", userId: 1, id: 1 }],
+    error: "",
+    loadingAll: false,
+    loadingModify: false,
+  },
+};
+
+export const testUseAppSelector = (f: (state: RootState) => void) => {
+  f(state);
+};
